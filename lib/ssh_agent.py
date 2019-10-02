de64fdff72facf15fc69156d7f56bb8c9498c97b
Mantas Mikulėnas <grawity@gmail.com>
2019-10-02 14:43:47 +0300

    split ssh-agent communications to their own module

---
+++ lib/ssh_agent.py
@@ -0,0 +1,91 @@
+import enum
+import socket
+
+class SshAgentCommand(enum.IntEnum):
+    REQUEST_IDENTITIES      = 11
+    SIGN_REQUEST            = 13
+    ADD_IDENTITY            = 17
+    REMOVE_IDENTITY         = 18
+    REMOVE_ALL_IDENTITIES   = 19
+
+class SshAgentReply(enum.IntEnum):
+    FAILURE                 = 5
+    SUCCESS                 = 6
+    IDENTITIES_ANSWER       = 12
+    SIGN_RESPONSE           = 14
+
+class SignRequestFlags(enum.IntFlag):
+    RSA_SHA2_256            = 1 << 1
+    RSA_SHA2_512            = 1 << 2
+
+class SshAgentKey(object):
+    def __init__(self, agent, keyblob, comment=None):
+        self.agent = agent
+        self.keyblob = keyblob
+        self.comment = comment
+
+        keydata = ssh_parse_publickey(self.keyblob, algoonly=True)
+        self.keyalgo = keydata["algo"]
+
+    def publickey_base64(self, with_type=False):
+        buf = binascii.b2a_base64(self.keyblob, newline=False).decode()
+        if with_type:
+            return self.keyalgo + " " + buf
+        else:
+            return buf
+
+    def fprint_md5_hex(self):
+        dgst = hashlib.md5(self.keyblob).digest()
+        dgst = ":".join(["%02x" % x for x in dgst])
+        return "MD5:" + dgst
+
+    def fprint_sha256_base64(self):
+        dgst = hashlib.sha256(self.keyblob).digest()
+        dgst = binascii.b2a_base64(dgst, newline=False).decode().rstrip("=")
+        return "SHA256:" + dgst
+
+    def sign_data(self, buf, flags=0):
+        return self.agent.sign_data(buf, self.keyblob, flags)
+
+class SshAgentConnection(object):
+    def __init__(self, path=None):
+        if not path:
+            path = os.environ["SSH_AUTH_SOCK"]
+        self.path = path
+        self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        self.sock.connect(self.path)
+        self.stream = SshStream(self.sock)
+
+    def list_keys(self):
+        self.stream.write_message([("byte", SshAgentCommand.REQUEST_IDENTITIES)])
+        pkt = self.stream.read_string_pkt()
+        result = SshAgentReply(pkt.read_byte())
+        if result != SshAgentReply.IDENTITIES_ANSWER:
+            raise IOError("expected IDENTITIES_ANSWER, got %r" % result)
+        nkeys = pkt.read_uint32()
+        keys = []
+        for i in range(nkeys):
+            keyblob = pkt.read_string()
+            comment = pkt.read_string().decode("utf-8")
+            key = SshAgentKey(self, keyblob, comment)
+            keys.append(key)
+        return keys
+
+    def get_key_by_fprint(self, fpr):
+        keys = self.list_keys()
+        for key in keys:
+            if fpr in {key.fprint_sha256_base64(), key.fprint_md5_hex()}:
+                return key
+        raise KeyError("no key with fingerprint %r found in agent" % fpr)
+
+    def sign_data(self, buf, keyblob, flags=0):
+        self.stream.write_message([("byte", SshAgentCommand.SIGN_REQUEST),
+                                   ("string", keyblob),
+                                   ("string", buf),
+                                   ("uint32", flags)])
+        pkt = self.stream.read_string_pkt()
+        result = SshAgentReply(pkt.read_byte())
+        if result != SshAgentReply.SIGN_RESPONSE:
+            raise IOError("expected SIGN_RESPONSE, got %r" % result)
+        sig = pkt.read_string()
+        return sig
